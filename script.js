8a473f55a50422ecb3f182ae84d2eecbed78af7d
fernanda-justus <fernanda.colen@escola.pr.gov.br>
2026-08-28 08:18:49 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botao = document.getElementById("btnMensagem");
+
+  botao.addEventListener("click", () => {
+    alert("Você clicou no botão!");
+  });
+});
